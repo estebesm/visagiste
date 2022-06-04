2dc1157382d494d3b20a023b5551818820eb3d5f
Estebes <estebesm95@gmail.com>
2022-06-05 01:27:24 +0600

    nothing

--- js/app.js
+++ js/app.js
@@ -9,34 +9,31 @@ const headerButtons = document.querySelectorAll('.header__nav-item')
 const headerBurger = document.querySelector('.header__burger')
 const headerCloseBtn = document.querySelector('.header__close-button')
 const headerLogo = document.querySelector('.header__logo')
+const headerNavLogo = document.querySelector('.header__nav__logo')
 
 function scrollTo(element) {
     window.scroll({
         behavior: 'smooth',
         left: 0,
-        top: element.offsetTop
+        top: element?.offsetTop
     });
 }
 
+function switchPage(btn){
+    if(isHeaderMenuOpened()){
+        closeHeaderMenu()
+    }
+    if(!document.getElementById('main__main').classList.contains('opened')){
+        openMain()
+    }
+    if(btn) scrollTo(document.getElementById(`main__${btn.id.split('__')[1].slice(0, -7)}`))
+    else scrollTo(document.getElementById(`top`))
+}
+
 const app = () => {
-    headerButtons.forEach(btn => btn.addEventListener('click', () => {
-        if(isHeaderMenuOpened()){
-            closeHeaderMenu()
-        }
-        if(!document.getElementById('main__main').classList.contains('opened')){
-            openMain()
-        }
-        scrollTo(document.getElementById(`main__${btn.id.split('__')[1].slice(0, -7)}`))
-    }))
-    headerLogo.addEventListener('click', () => {
-        if(isHeaderMenuOpened()){
-            closeHeaderMenu()
-        }
-        if(!document.getElementById('main__main').classList.contains('opened')){
-            openMain()
-        }
-        scrollTo(document.getElementById(`main__${btn.id.split('__')[1].slice(0, -7)}`))
-    })
+    headerButtons.forEach(btn => btn.addEventListener('click',() => switchPage(btn)))
+    headerLogo.addEventListener('click', () => switchPage())
+    headerNavLogo.addEventListener('click', () => switchPage())
     main.addEventListener('click', openMain)
     services.forEach((item, index) => {
         item.addEventListener('click', () => {
