9fde00c485d0d3659ea90e4035bbe46d92967359
Estebes <estebesm95@gmail.com>
2022-07-25 17:12:35 +0600

    updated call button function

--- js/services.js
+++ js/services.js
@@ -2,6 +2,23 @@ import {closeMain, openMain} from "./main.js";
 
 const services = document.querySelector('.services')
 
+const callButtons = document.querySelectorAll('.call-button')
+
+function scrollTo(element) {
+    window.scroll({
+        behavior: 'smooth',
+        left: 0,
+        top: element?.offsetTop
+    });
+}
+
+callButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        openMain()
+        scrollTo(document.getElementById(`main__feedback`))
+    })
+})
+
 export const openServices = (index) => {
     closeMain()
     services.classList.add('opened')
